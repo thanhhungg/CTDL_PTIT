25d77a2fab79f1435ebb0dc42af373591549005b
thanhhungg <hungnt.b20cn342@stu.ptit.vn>
2022-02-08 13:25:31 +0700

    Code

--- CTDL_PTIT/DSA06002.cpp
+++ CTDL_PTIT/DSA06002.cpp
@@ -1,61 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define endl "\n"
+using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-    //cách 1:
-        // int n,k,a[1000],b[1000];
-        // cin>>n>>k;
-        // for(int i=0;i<n;i++)
-        // {
-        //     cin>>a[i];
-        //     b[i]=abs(k-a[i]);
-        // }
-        // for(int i=0;i<n-1;i++)
-        // {
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         if(b[i]>b[j]) 
-        //         {
-        //             swap(a[i],a[j]);
-        //             swap(b[i],b[j]);
-        //         }
-        //     }
-        // }
-        // for(int i=0;i<n;i++)
-        // {
-        //     cout<<a[i]<<" ";
-        // }
-        // cout<<endl;
-    //cách 2:
-        int n,k;
-        cin>>n>>k;
-        pair<int ,int > a[100];
-        for(int i=0;i<n;i++)
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        pair<int, int> b[n];
+        for (int i = 0; i < n; i++)
         {
-            int x;
-            cin>>x;
-            a[i]={x,abs(k-x)};
+            cin >> a[i];
+            b[i].first = abs(a[i] - k);
+            b[i].second = i;
         }
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(a[i].second>a[j].second) swap(a[i],a[j]);
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            cout<<a[i].first<<" ";
-        }
-        cout<<endl;
-
+        sort(b,b+n);
+        for (int i = 0; i < n; i++)
+            cout << a[b[i].second]<< " ";
+        cout << endl;
     }
 }
